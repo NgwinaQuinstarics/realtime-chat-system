21ce54c7563874655b6945d92747c114df885285
quinstarics24 <quinngwina@gmail.com>
2026-04-28 08:47:15 +0100

    Setup basic Express server with middleware

--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,13 @@ const app = express();
 
 const PORT = 5000;
 
+// Middleware (important for later)
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Server is running 🚀');
+  res.send('API is running 🚀');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
